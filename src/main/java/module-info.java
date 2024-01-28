c0d43b7a9e1014b4b6315db6ead3a45da50af03b
Oliver Kopp <kopp.dev@gmail.com>
2024-01-28 01:50:07 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+open module org.jabref {
+    requires org.slf4j;
+}
---
+++ src/main/java/org/jabref/cli/Launcher.java
@@ -0,0 +1,11 @@
+package org.jabref.cli;
+
+import org.slf4j.Logger;
+
+public class Launcher {
+    private static Logger LOGGER;
+
+    public static void main(String[] args) {
+        LOGGER.info("Started main");
+    }
+}
